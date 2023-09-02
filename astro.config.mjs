7e3cb279e5eebe72e45d6f36b399a7224f7259f0
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2023-09-01 19:34:12 -0700

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    redirects: {
+        '/Sign-Up': 'https://go.graphics-for-good.com/Sign-Up',
+    }
+});
