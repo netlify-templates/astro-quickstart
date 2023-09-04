4bf03ca0387fd52765bab6be20bdf3fb100b0ad1
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2023-09-03 18:01:36 -0700

    update header.js

--- public/header.js
+++ public/header.js
@@ -1,4 +1,4 @@
-let isSmall = false
+let isSmallScreen = false
 let nav = document.querySelector('.header > .nav')
 let btnsDiv = nav.querySelector('div')
 let openSVG = nav.querySelector('.nav-hbg')
@@ -12,9 +12,9 @@ closeSVG.addEventListener('click', function(e) {
 })
 window.addEventListener('resize', function(e) {
   let width = window.innerWidth
-  if (isSmall === false && width < 624) isSmall = true
+  if (isSmallScreen === false && width < 624) isSmallScreen = true
   if (width >= 624) {
-    isSmall = false
-    if (isSmall === false) btnsDiv.style.gridTemplateColumns = '0fr'
+    isSmallScreen = false
+    if (isSmallScreen === false) btnsDiv.style.gridTemplateColumns = '0fr'
   }
 })
