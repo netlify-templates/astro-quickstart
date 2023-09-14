54f3043a162be299d12dfa3d1095fff53e6f28da
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2023-09-13 18:42:59 -0700

    update adarsh.astro

--- src/pages/members/defaultBio.js
+++ src/pages/members/defaultBio.js
@@ -1,4 +1,4 @@
-function defaultBio(w1, w2, role) {
+function defaultBio(name, w1, w2, role) {
     if (!!w1 === false) {
       w1 = 'a'
     }
@@ -6,22 +6,21 @@ function defaultBio(w1, w2, role) {
       w2 = 'of'
     }
     if (!!user) {
-      if (!!mname === false) {
-        mname = ''
+      if (!!name === false) {
+        name = ''
       }
-      var uName = mname
-      if (uName.includes(' <span id="tag">') || !!role) {
+      if (name.includes(' <span id="tag">') || !!role) {
         if (!!role === false) {
-          role = uName.split(' <span id="tag">(')[1].split(')</span>')[0].replace(',', '')
+          role = name.split(' <span id="tag">(')[1].split(')</span>')[0].replace(',', '')
         }
-        return `${uName.split(' <span id="tag">')[0].split(' ')[0]}{pronouciation} is ${w1} ${role} ${w2} the Graphics for Good club at <a href="https://dtechhs.org">Design Tech High School</a> in Redwood City.`
+        return `${name.split(' <span id="tag">')[0].split(' ')[0]}{pronouciation} is ${w1} ${role} ${w2} the Graphics for Good club at <a href="https://dtechhs.org">Design Tech High School</a> in Redwood City.`
       }
       else {
-        if (uName.includes('(') && uName.includes(')')) {
-          uName = uName.split('(')[1].split(')')[0]
+        if (name.includes('(') && name.includes(')')) {
+          name = name.split('(')[1].split(')')[0]
         }
         role = 'Member'
-        return `${uName.split(' ')[0]}{pronouciation} is ${w1} ${role} ${w2} the Graphics for Good club at <a href="https://dtechhs.org">Design Tech High School</a> in Redwood City.`
+        return `${name.split(' ')[0]}{pronouciation} is ${w1} ${role} ${w2} the Graphics for Good club at <a href="https://dtechhs.org">Design Tech High School</a> in Redwood City.`
       }
     }
     else {
