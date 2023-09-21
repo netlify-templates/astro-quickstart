17d8c864763f8a0d03b60b3f4ad77075a4a6dd39
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2023-09-21 16:51:51 -0700

    update eruda.js

--- public/eruda.js
+++ public/eruda.js
@@ -1,6 +1,1 @@
 var script = document.createElement('script'); script.src="//cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); script.onload = function () { eruda.init() };
-setTimeout(function() {
-  if (!!document.getElementById('eruda').getAttribute('style')) {
-    document.getElementById('eruda').removeAttribute('style')  
-  }
-}, 100)
