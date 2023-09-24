c4b82f28a2fa1b697353bdbb3323aced9d5b07ce
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2023-09-24 10:25:39 -0700

    updaet projects.js

--- public/projects.js
+++ public/projects.js
@@ -9,30 +9,28 @@ function showItem(path, c, element='img', capt=false, desc=false, user=false) {
   showModal(element, path, capt, desc)
 }
 
-if (location.pathname === '/') {
-  document.querySelectorAll('main .split > section img').forEach(e => {
-    let path = e.scroll
-    if (path.includes('://')) {
-      path = path.split('://')[1]
-      if (path.split('/')[0] === location.host) {
-        path = path.split('/')
-        path.shift()
-        path = path.join('/')
-      }
+document.querySelectorAll('main .split > section img').forEach(e => {
+  let path = e.src
+  if (path.includes('://')) {
+    path = path.split('://')[1]
+    if (path.split('/')[0] === location.host) {
+      path = path.split('/')
+      path.shift()
+      path = path.join('/')
     }
+  }
     
-    let capt = `${e.alt}. Click to enlarge!`.replace('.. ', '. ').replace('..', '.').replace('!. ', '! ').replace('!.', '!')
-    e.title = capt
+  let capt = `${e.alt}. Click to enlarge!`.replace('.. ', '. ').replace('..', '.').replace('!. ', '! ').replace('!.', '!')
+  e.title = capt
     
-    let element = e.getAttribute('data-element')
-    if (!!element === false) {
-      element = 'img'
-    }
+  let element = e.getAttribute('data-element')
+  if (!!element === false) {
+    element = 'img'
+  }
 
-    e.addEventListener('click', function(event) {
-      showItem(path, CLUB_PHOTOS['path'], element)
-    })
+  e.addEventListener('click', function(event) {
+    showItem(path, CLUB_PHOTOS['path'], element)
   })
-}
+})
 
 //document.querySelector('[tElement]').innerHTML = ''
