49cf10f041707388469be029f312a613fb6b1e1e
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2023-09-25 19:11:49 -0700

    create and use debug.astro

--- public/eruda.js
+++ public/eruda.js
@@ -1,1 +1,35 @@
-var script = document.createElement('script'); script.src="//cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); script.onload = function () { eruda.init() };
+if (!!window) {
+    if (parent) {
+        var pLocation = parent.location
+        if (!!pLocation) {
+            var pLocationPathname = pLocation.pathname
+            if (!!pLocationPathname) {
+                if (!!pLocationPathname.includes(':///')) {
+                    pLocationPathname = pLocationPathname.split('://')[1]
+                }
+                if (!!pLocationPathname.includes('/')) {
+                    pLocationPathname = pLocationPathname.split('/')
+                    var canProceed = false
+                    if (pLocationPathname.length < 1) {
+                        if (pLocationPathname[1] === 'debug') canProceed = true
+                    }
+                    else {
+                        pLocationPathname.forEach(function(p, i) {
+                            if (p === 'debug') canProceed = true
+                        })
+                    }
+                    if (!!canProceed) showEruda()
+                }
+            }
+        }
+    }
+}
+
+function showEruda() {
+    var script = document.createElement('script');
+    script.src="//cdn.jsdelivr.net/npm/eruda";
+    document.body.appendChild(script);
+    script.addEventListener.onload = function () {
+        eruda.init()
+    };
+}
