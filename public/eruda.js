d9202ce8f5a33f460163161822c7066a098d8604
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2023-10-20 11:50:16 -0700

    update layout.astro

--- public/eruda.js
+++
@@ -1,6 +0,0 @@
-var script = document.createElement('script');
-script.src="//cdn.jsdelivr.net/npm/eruda";
-document.body.appendChild(script);
-script.addEventListener.onload = function () {
-    eruda.init()
-};
