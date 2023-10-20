3b764d504bb35a959dc5439df678eb7d560bd58b
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2023-10-20 09:38:35 -0700

    update defaultBio.js

--- src/pages/members/defaultBio.js
+++ src/pages/members/defaultBio.js
@@ -12,14 +12,14 @@ function defaultBio(name, w1, w2, role) {
     if (!!role === false) {
       role = name.split(' <span id="tag">(')[1].split(')</span>')[0].replace(',', '')
     }
-    return `${name.split(' <span id="tag">')[0].split(' ')[0]}{pronunciation} is ${w1} ${role} ${w2} the Graphics for Good club at <a href="https://dtechhs.org">Design Tech High School</a> in Redwood City.`
+    return `${name.split(' <span id="tag">')[0]}{pronunciation} is ${w1} ${role} ${w2} the Graphics for Good club at <a href="https://dtechhs.org">Design Tech High School</a> in Redwood City.`
   }
   else {
     if (name.includes('(') && name.includes(')')) {
       name = name.split('(')[1].split(')')[0]
     }
     role = 'Member'
-    return `${name.split(' ')[0]}{pronunciation} is ${w1} ${role} ${w2} the Graphics for Good club at <a href="https://dtechhs.org">Design Tech High School</a> in Redwood City.`
+    return `${name}{pronunciation} is ${w1} ${role} ${w2} the Graphics for Good club at <a href="https://dtechhs.org">Design Tech High School</a> in Redwood City.`
   }
 }
   
