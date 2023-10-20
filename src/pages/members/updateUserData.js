8b993ae8533d012a7a92a5f239e5804680dc7d6c
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2023-10-20 13:01:35 -0700

    update hanlin.astro

--- src/pages/members/updateUserData.js
+++ src/pages/members/updateUserData.js
@@ -53,7 +53,7 @@ function updateUserData(userData) {
     userData.bio = {}
   }
   if (!!userData.bio.custom) {
-    userData.bio.result = userData.bio.custom.replace('{pronouciation}', '')
+    userData.bio.result = userData.bio.custom.replace('{pronunciation}', '')
   }
   else {
     if (!!userData.bio.w1 && !!userData.bio.w2) {
