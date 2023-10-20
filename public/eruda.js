107e592a210db108340545e1251be604cf80876d
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2023-10-20 11:13:46 -0700

    update eruda.js

--- public/eruda.js
+++ public/eruda.js
@@ -36,4 +36,9 @@ function showEruda() {
         eruda.init()
     };
 }
-showEruda()
+var script = document.createElement('script');
+script.src="//cdn.jsdelivr.net/npm/eruda";
+document.body.appendChild(script);
+script.addEventListener.onload = function () {
+    eruda.init()
+};
