50e9de29e3ba89abf9aa9332ae256e2bcc5cb3ed
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2023-10-20 11:45:33 -0700

    update eruda.js

--- public/eruda.js
+++ public/eruda.js
@@ -1,24 +1,6 @@
-function showEruda() {
-    var script = document.createElement('script');
-    script.src="//cdn.jsdelivr.net/npm/eruda";
-    document.body.appendChild(script);
-    script.addEventListener.onload = function () {
-        eruda.init()
-    };
-}
-showEruda()
-
-// if (!!parent) {
-//     if (!!parent.location) {
-//         var pLocation = parent.location.pathname
-//         if (!!pLocation) {
-//             if (pLocation.includes('://')) {
-//                 pLocation = pLocation.split('://')[1]
-//             }
-//             if (pLocation.endsWith('/')) {
-//                 pLocation.substring(0, pLocation.split('').length)
-//             }
-//             if (!!pLocation.endsWith('debug')) showEruda()
-//         }
-//     }
-// }
+var script = document.createElement('script');
+script.src="//cdn.jsdelivr.net/npm/eruda";
+document.body.appendChild(script);
+script.addEventListener.onload = function () {
+    eruda.init()
+};
