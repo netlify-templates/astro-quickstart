2b3dcfb991895972b7a2d11febfab2cbb0b75b14
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2023-10-20 11:18:34 -0700

    update eruda.js

--- public/eruda.js
+++ public/eruda.js
@@ -8,32 +8,17 @@ function showEruda() {
 }
 showEruda()
 
-if (!!window) {
-    if (!!parent) {
-        var pLocation = parent.location
-        if (!!pLocation) {
-            var pLocationPathname = pLocation.pathname
-            if (!!pLocationPathname) {
-                if (pLocationPathname.includes('://')) {
-                    pLocationPathname = pLocationPathname.split('://')[1]
-                }
-                if (pLocationPathname.includes('/')) {
-                    pLocationPathname = pLocationPathname.split('/')[1]
-                    if (pLocationPathname.endsWith('/')) {
-                        pLocationPathname.substring(0, pLocationPathname.split('').length)
-                    }
-                    var canProceed = false
-                    if (pLocationPathname.length < 1) {
-                        if (pLocationPathname[1] === 'debug') canProceed = true
-                    }
-                    else {
-                        pLocationPathname.forEach(function(p, i) {
-                            if (p === 'debug') canProceed = true
-                        })
-                    }
-                    if (!!canProceed) showEruda()
-                }
+if (!!parent) {
+    if (!!parent.location) {
+        if (!!parent.location.pathname) {
+            var location = parent.location.pathname
+            if (location.includes('://')) {
+                location = location.split('://')[1]
             }
+            if (location.endsWith('/')) {
+                location.substring(0, location.split('').length)
+            }
+            if (!!location.endsWith('debug')) showEruda()
         }
     }
 }
