04fbd6a9ab792af9c75fd0671f8b10aae449334d
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2023-12-12 12:44:34 -0800

    update portalpw.js

--- public/portalpw.js
+++ public/portalpw.js
@@ -9,32 +9,25 @@ if (Boolean(urlParam.get('i')) === true && localStorage.getItem('p') === correct
 else {
   user_password = btoa(prompt('Enter Password: '));
 }
-var HTML = atob('PGhlYWRlciBzdHlsZT0ibWluLWhlaWdodDogMTAwcHggIWltcG9ydGFudDsgZGlzcGxheTogZmxleDsgYWxpZ24taXRlbXM6IGNlbnRlcjsiPjxoMT5NZW1iZXIgUG9ydGFsPC9oMT48L2hlYWRlcj48bWFpbiBpZD0icG9ydGFsIj48L21haW4+')
-if (user_password === correct) {
-  let header = document.createElement('header')
-  header.style = 'min-height: 100px !important; display: flex; align-items: center;'
+
+var xhr = new XMLHttpRequest()
+xhr.open('GET', `/portal/landing/`)
+xhr.addEventListener('load', xhrAct())
+xhr.send()
+
+function xhrAct() {
+  var HTML = atob(this.responseText)
+  if (user_password === correct) {
+    document.body.innerHTML += HTML
   
-  let section = document.createElement('section')
-  section.id = 'content'
-  header.appendChild(section)
-
-  let h1 = document.createElement('h1')
-  h1.innerHTML = 'Member Portal'
-  section.appendChild(h1)
-
-  let main = document.createElement('main')
-  main.id = 'portal'
-
-  document.body.appendChild(header)
-  document.body.appendChild(main)
-
-  let script = document.createElement('script')
-  script.src = '/portal.js'
-  document.body.appendChild(script)
-  document.documentElement.className = 'fullHeight'
-  document.body.className = 'fullHeight'
-}
-else {
-  history.back()
-  location.href = '/wrong-portal-password'
+    let script = document.createElement('script')
+    script.src = '/portal.js'
+    document.body.appendChild(script)
+    // document.documentElement.className = 'fullHeight'
+    // document.body.className = 'fullHeight'
+  }
+  else {
+    history.back()
+    location.href = '/wrong-portal-password'
+  }
 }
