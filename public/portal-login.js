43ed98d1497fcb3091c99882a90075881e227095
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2023-12-12 12:32:28 -0800

    create and use portal/landing.astro

--- public/portal-login.js
+++
@@ -1,4 +0,0 @@
-const express = require('express'); // Include ExpressJS
-const app = express(); // Create an ExpressJS app
-const bodyParser = require('body-parser'); // middleware
-app.use(bodyParser.urlencoded({ extended: false }));
