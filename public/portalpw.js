615d266781eb74147352cef30a76f67a3a479335
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2023-12-13 09:39:20 -0800

    update portalpw.js

--- public/portalpw.js
+++ public/portalpw.js
@@ -24,7 +24,7 @@ function xhrAct() {
     var footer = document.querySelector('footer')
     var newPortal = portal.cloneNode(true)
     portal.remove()
-    footer.insertAdjacentElement('afterend', newPortal)
+    footer.insertBefore('afterend', newPortal)
   
     let script = document.createElement('script')
     script.src = '/portal.js'
