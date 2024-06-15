c27fb2cb4c2f797f5a0ae4b35f7fe7786137d8a5
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-06-15 22:30:09 +0000

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 
 export default defineConfig({
     trailingSlash: "ignore",
-    output: 'server',
-    adapter: node({
-        mode: 'standalone',
-    }),
 });
