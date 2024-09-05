d0e77163c1f2ebef906fc6223cd7e18b489791de
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-09-05 13:22:10 -0700

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
     site: 'https://graphics-for-good.com',
     trailingSlash: "ignore",
-    // integrations: [sitemap()],
+    integrations: [sitemap()],
 });
