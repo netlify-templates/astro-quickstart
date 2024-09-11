8a97b7ce475f167fd5faec3892f562a96691abf6
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-09-11 11:01:03 -0700

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,31 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import path from 'path';
+import fs from 'fs';
+
+const siteUrl = 'https://graphics-for-good.com';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const directoryPath = path.join(__dirname, 'members');
+const files = fs.readdirSync(directoryPath);
+
+const memberUrls = []
+files.forEach((file) => {
+    var isDir = fs.lstatSync(`${directoryPath}/${file}`).isDirectory()
+    if (isDir) {
+        const fileName = file.split('.')[0];
+        memberUrls.push(`${siteUrl}/members/${fileName}`);
+    }
+});
 
 export default defineConfig({
-    site: 'https://graphics-for-good.com',
+    site: siteUrl,
     trailingSlash: "ignore",
-    integrations: [sitemap()],
+    integrations: [sitemap({
+        customPages: [].concat(memberUrls),
+        priority: 0.5,
+        changefreq: 'monthly'
+    })],
 });
