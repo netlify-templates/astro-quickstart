b516dfd279617a6711d485ddc73bdac4b93d6ec8
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-09-25 17:48:21 -0700

    create and host david/info.js

---
+++ members/david/info.js
@@ -0,0 +1,35 @@
+var data = {
+    "status": "active", 
+    "username": "david", 
+    "name": "David Rojas",
+    "role": false,
+    "pronunciation": false,
+    "email": {
+        "g4g": false,
+        "dtech": {
+            "year": 28
+        }
+    },
+    "pfp": {
+        "g4g": false,
+        "google": {
+            "a": "a-", 
+            "url": "ALV-UjVQPIY3wF8hbM7yabASZHoNfxvWtcBrTbw1mY0in4iT9qyIfUk"
+        }
+    },
+    "location": {
+        "city": "Bay Area", 
+        "state": ""
+    },
+    "bio": {
+
+    },
+    "socials": [
+        // {
+        //     "url": "https://instagram.com/4goodgraphics", 
+        //     "name": "Instagram"
+        // }
+    ]
+}
+  
+export default data
