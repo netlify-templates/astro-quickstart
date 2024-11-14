55f7a9d9cf5d28422975233399d909fe4029f018
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-11-14 10:16:13 -0800

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,6 +20,7 @@ members.forEach((username, i) => {
                 if (info.endsWith(`export default data`)) info = info.slice(0, -1*'export default data'.length)
                 while (info.endsWith(`\n`)) info = info.slice(0, -2)
                 if (info.startsWith('{') && info.endsWith('}')) {
+                    if (i === 0) console.log('hi')
                     info = JSON.parse(info)
                     info.username = username
                     
