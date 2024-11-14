ccbba9586920bf8bf4f92d020494713b0a2a4baa
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-11-13 22:17:13 -0800

    update [username].astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,32 +5,32 @@ import fs from 'fs';
 
 const siteUrl = 'https://graphics-for-good.com';
 
-const directoryPath = path.join('./', 'members');
-const members = fs.readdirSync(directoryPath);
+// const directoryPath = path.join('./', 'members');
+// const members = fs.readdirSync(directoryPath);
 
-members.forEach((username, i) => {
-    var isDir = fs.lstatSync(`${directoryPath}/${username}`).isDirectory()
-    if (isDir) {
-        var info = `${directoryPath}/${username}/info.js`
-        if (fs.existsSync(info)) {
-            info = fs.readFileSync(info, 'utf-8')
-            if (info) {
-                if (info.startsWith('var data = ')) info = info.slice('var data = '.length)
-                // while (info.startsWith(`\n`)) info = info.slice('\n'.length)
-                if (info.endsWith(`export default data`)) info = info.slice(0, -1*'export default data'.length)
-                while (info.endsWith(`\n`)) info = info.slice(0, -2)
-                if (info.startsWith('{') && info.endsWith('}')) {
-                    info = JSON.parse(info)
-                    info.username = username
+// members.forEach((username, i) => {
+//     var isDir = fs.lstatSync(`${directoryPath}/${username}`).isDirectory()
+//     if (isDir) {
+//         var info = `${directoryPath}/${username}/info.js`
+//         if (fs.existsSync(info)) {
+//             info = fs.readFileSync(info, 'utf-8')
+//             if (info) {
+//                 if (info.startsWith('var data = ')) info = info.slice('var data = '.length)
+//                 // while (info.startsWith(`\n`)) info = info.slice('\n'.length)
+//                 if (info.endsWith(`export default data`)) info = info.slice(0, -1*'export default data'.length)
+//                 while (info.endsWith(`\n`)) info = info.slice(0, -2)
+//                 if (info.startsWith('{') && info.endsWith('}')) {
+//                     info = JSON.parse(info)
+//                     info.username = username
                     
-                    info = JSON.stringify(info, null, 2)
-                    info = `var data = ${info}\n\nexport default data`
-                    fs.writeFileSync( `${directoryPath}/${username}/info.js`, info)
-                }
-            }
-        }
-    }
-});
+//                     info = JSON.stringify(info, null, 2)
+//                     info = `var data = ${info}\n\nexport default data`
+//                     fs.writeFileSync( `${directoryPath}/${username}/info.js`, info)
+//                 }
+//             }
+//         }
+//     }
+// });
 
 export default defineConfig({
     site: siteUrl,
