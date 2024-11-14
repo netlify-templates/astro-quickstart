f305b4a62ac002cbb675ba93d2b58b4b8e22dbbc
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-11-14 09:57:23 -0800

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,32 +5,26 @@ import fs from 'fs';
 
 const siteUrl = 'https://graphics-for-good.com';
 
-// const directoryPath = path.join('./', 'members');
-// const members = fs.readdirSync(directoryPath);
+const directoryPath = path.join('./', 'members');
+const members = fs.readdirSync(directoryPath);
 
-// members.forEach((username, i) => {
-//     var isDir = fs.lstatSync(`${directoryPath}/${username}`).isDirectory()
-//     if (isDir) {
-//         var info = `${directoryPath}/${username}/info.js`
-//         if (fs.existsSync(info)) {
-//             info = fs.readFileSync(info, 'utf-8')
-//             if (info) {
-//                 if (info.startsWith('var data = ')) info = info.slice('var data = '.length)
-//                 // while (info.startsWith(`\n`)) info = info.slice('\n'.length)
-//                 if (info.endsWith(`export default data`)) info = info.slice(0, -1*'export default data'.length)
-//                 while (info.endsWith(`\n`)) info = info.slice(0, -2)
-//                 if (info.startsWith('{') && info.endsWith('}')) {
-//                     info = JSON.parse(info)
-//                     info.username = username
-                    
-//                     info = JSON.stringify(info, null, 2)
-//                     info = `var data = ${info}\n\nexport default data`
-//                     fs.writeFileSync( `${directoryPath}/${username}/info.js`, info)
-//                 }
-//             }
-//         }
-//     }
-// });
+members.forEach((username, i) => {
+    var isDir = fs.lstatSync(`${directoryPath}/${username}`).isDirectory()
+    if (isDir) {
+        var portfolio = []
+        if (fs.existsSync(`${directoryPath}/${username}/portfolio/`)) {
+            if (fs.lstatSync(`${directoryPath}/${username}/portfolio/`).isDirectory()) {
+                portfolio = fs.readdirSync(`${directoryPath}/${username}/portfolio/`)
+            }
+        }
+
+        portfolio = {
+            username: username, 
+            items: portfolio
+        }
+        fs.writeFileSync( `${directoryPath}/${username}/portfolio.js`, JSON.stringify(portfolio, null, 4))
+    }
+});
 
 export default defineConfig({
     site: siteUrl,
